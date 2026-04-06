4e65c71af8d2b3da4b20af70ee766074affdbc90
Edgarin <maed2802@gmail.com>
2026-04-06 11:47:29 -0600

    Variables

---
+++ Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string cadena = "Esto es una cadena";
+Console.WriteLine(cadena);
+
+cadena = string.Empty;
+Console.WriteLine(cadena);
+
+Console.WriteLine("Escriba su nombre por consola");
+
+string tuNombre = Console.ReadLine()!;
+
+Console.WriteLine("Tu nombre es: " + tuNombre);
+
+Console.WriteLine("Escribe tu edad");
+string entradaEdad = Console.ReadLine()!;
+int edad = Convert.ToInt32(entradaEdad);
+Console.WriteLine("Tu edad es : " + entradaEdad + 
+" y tu nombre es: " + tuNombre);
