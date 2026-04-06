ebfb3d6e81cfc91182cfc7e5a8a4880eacd8c589
Edgarin <maed2802@gmail.com>
2026-04-06 12:27:32 -0600

    Curso básico Chi Charp numbers

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/*
 string cadena = "Esto es una cadena";
 Console.WriteLine(cadena);
 
 cadena = string.Empty;
 Console.WriteLine(cadena);
+
 
 Console.WriteLine("Escriba su nombre por consola");
 
@@ -14,5 +17,38 @@ Console.WriteLine("Tu nombre es: " + tuNombre);
 Console.WriteLine("Escribe tu edad");
 string entradaEdad = Console.ReadLine()!;
 int edad = Convert.ToInt32(entradaEdad);
-Console.WriteLine("Tu edad es : " + entradaEdad + 
+Console.WriteLine("Tu edad es : " + edad + 
 " y tu nombre es: " + tuNombre);
+
+//Diferentes tipos de concatenación con string
+string nombre = "Edgar";
+string apellido = "Juárez";
+
+
+Console.WriteLine(nombre +" "+ apellido);
+Console.WriteLine($"Nombre: {nombre}, apellido: {apellido}");
+Console.WriteLine("Nombre {0}, apellido {1}", nombre, apellido);
+
+*/
+
+Console.WriteLine("------Números-------");
+
+int numero1= 23;
+int numero2 = 10;
+int resultado = numero1 + numero2;
+
+
+Console.WriteLine(resultado);
+Console.WriteLine("resultado: " + Convert.ToInt32(numero1 + numero2));
+
+Console.WriteLine(numero1 - numero2);
+Console.WriteLine(numero1 / numero2);
+Console.WriteLine(numero1 * numero2);
+
+Console.WriteLine("**********Decimales****");
+
+float f = 1f /3f; 
+
+//ejejmplo a los float hay que ponerles una f al final
+ float sueldo = 1220.50f;
+ Console.WriteLine(sueldo);
--- Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
+++ Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e65c71af8d2b3da4b20af70ee766074affdbc90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
