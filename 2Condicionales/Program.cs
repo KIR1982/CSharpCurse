ca5ddfb83dac99c03fd51a3c1890ccca6d86d4f0
Edgarin <maed2802@gmail.com>
2026-04-08 16:31:23 -0600

    Condicionales

---
+++ 2Condicionales/Program.cs
@@ -0,0 +1,51 @@
+﻿// Condicionales y operadores
+
+int edadPersona1 = 56;
+
+int  edadPersona2 = 75;
+
+
+
+if(edadPersona1 < edadPersona2)
+{
+    Console.WriteLine("Si es verdad");
+}
+
+string sexoPersona1 = "hombre";
+
+if (edadPersona1 > edadPersona2  || sexoPersona1 == "hombre")
+{
+    Console.WriteLine("Es la onda");
+    
+}
+
+bool usuarioAlta = true;
+
+if (usuarioAlta)
+{
+    Console.WriteLine("Si es alta");
+}
+
+Console.WriteLine("switch");
+
+string nacionalidad = "español";
+
+if (nacionalidad == "mexicano")
+{
+    Console.WriteLine("Es mexicano");
+}
+
+
+switch (nacionalidad)
+{
+    case "uruguayo":
+    Console.WriteLine("Es uruguayo");
+    break;
+
+    case "mexicano":
+    Console.WriteLine("Es mexicano");
+    break;
+    default:
+    Console.WriteLine("Sin nacionalidad");
+    break;
+}
